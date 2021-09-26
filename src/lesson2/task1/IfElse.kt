84331ae9d7f2b1e9de2d206786d1963a6fd36e0c
Nikanorenkov-Mihail <nikanorenkov.md@edu.spbstu.ru>
2021-09-27 00:18:19 +0300

    Update IfElse.kt

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -7,11 +7,6 @@ import kotlin.math.max
 import kotlin.math.sqrt
 import kotlin.math.min
 import kotlin.math.abs
-// Урок 2: ветвления (здесь), логический тип (см. 2.2).
-// Максимальное количество баллов = 6
-// Рекомендуемое количество баллов = 5
-// Вместе с предыдущими уроками = 9/12
-
 /**
  * Пример
  *
@@ -45,22 +40,22 @@ fun gradeNotation(grade: Int): String = when (grade) {
  * Найти наименьший корень биквадратного уравнения ax^4 + bx^2 + c = 0
  */
 fun minBiRoot(a: Double, b: Double, c: Double): Double {
-    // 1: в главной ветке if выполняется НЕСКОЛЬКО операторов
+
     if (a == 0.0) {
-        if (b == 0.0) return Double.NaN // ... и ничего больше не делать
+        if (b == 0.0) return Double.NaN
         val bc = -c / b
-        if (bc < 0.0) return Double.NaN // ... и ничего больше не делать
+        if (bc < 0.0) return Double.NaN
         return -sqrt(bc)
-        // Дальше функция при a == 0.0 не идёт
+
     }
-    val d = discriminant(a, b, c)   // 2
-    if (d < 0.0) return Double.NaN  // 3
+    val d = discriminant(a, b, c)
+    if (d < 0.0) return Double.NaN
     // 4
     val y1 = (-b + sqrt(d)) / (2 * a)
     val y2 = (-b - sqrt(d)) / (2 * a)
-    val y3 = max(y1, y2)       // 5
-    if (y3 < 0.0) return Double.NaN // 6
-    return -sqrt(y3)           // 7
+    val y3 = max(y1, y2)
+    if (y3 < 0.0) return Double.NaN
+    return -sqrt(y3)
 }
 
 /**
@@ -70,12 +65,7 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
 fun ageDescription(age: Int): String {
-    /*
-    if (age % 100 >= 10 && age % 100 <= 20) return ("$age лет")
-    else if (age % 10 == 0 || age % 10 == 5 || age % 10 == 6 || age % 10 == 7 || age % 10 == 8 || age % 10 == 9) return ("$age лет")
-    else if (age % 10 == 1 ) return ("$age год")
-    else return ("$age годa")
-     */
+
     if (age % 100 >= 10 && age % 100 <= 20) return ("$age лет")
     return when (age % 10 ) {
         1 -> "$age год"
@@ -97,15 +87,15 @@ fun timeForHalfWay(
     t2: Double, v2: Double,
     t3: Double, v3: Double
 ): Double {
-    var time :Double = t1+t2+t3
-    var s : Double = (t1 * v1) + (t2 * v2) + (t3 * v3)
+    var time = t1+t2+t3
+    var s = t1 * v1 + t2 * v2 + t3 * v3
 
-    var s2:Double = ((v1*t1) + (v2*t2) + (v3*t3))/2
-    if ((t1*v1) < s2){
-        if (((t1*v1)+(t2*v2)) < s2){
-            return t1+t2+(s2-((v1*t1) + (v2*t2) ))/v3
+    var s2:Double = (v1*t1 + v2*t2 + v3*t3)/2
+    if (t1*v1 < s2){
+        if ((t1*v1+t2*v2) < s2){
+            return t1+t2+(s2-(v1*t1 + v2*t2))/v3
         }
-        else return t1+(s2-(v1*t1))/v2
+        else return t1+(s2-v1*t1)/v2
     }
     else return s2/v1
 }
@@ -182,13 +172,13 @@ fun triangleKind(a: Double, b: Double, c: Double): Int {
  * Если пересечения нет, вернуть -1.
  */
 fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
-    if (c > b && c > a) return -1 //отрезки друг за другом
-    else if (c == b && c > a ) return 0 // пересекаются в точке с, в
-    if (a <c && d< b) return abs(d-c) //cd вложен
-    else if (c < b && c > a) return abs(b-c) // отрезок от в до с
-    if (a>c && a>d) return -1 // cd до ав
-    if (c<a && b<d) return abs(b-a) //пересечение ab
-    if (c<a && b>d) return abs(d-a) //пересечение ad
+    if (c > b && c > a) return -1
+    else if (c == b && c > a ) return 0
+    if (a <c && d< b) return abs(d-c)
+    else if (c < b && c > a) return abs(b-c)
+    if (a>c && a>d) return -1
+    if (c<a && b<d) return abs(b-a)
+    if (c<a && b>d) return abs(d-a)
     else if (a == c && b == d) return abs(b-a)
     if ((a==c && b>d))return abs(d-a)
     if ((a==c && b<d))return abs(b-a)
