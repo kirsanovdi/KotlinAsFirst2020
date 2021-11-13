cf8a93c5de8b5ef732c5019ce0b3d9701467eafa
kirsanovdi <kirsanov.di@edu.spbstu.ru>
2021-11-13 16:45:24 +0300

    lesson 7

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -6,12 +6,13 @@ import lesson1.task1.sqr
 import java.io.File
 import java.util.*
 import kotlin.math.*
+import kotlin.random.Random.Default.nextDouble
 
 const val delta = 1e-10
 
 data class Point(val x: Double, val y: Double) {
     fun distance(other: Point): Double = sqrt(sqr(x - other.x) + sqr(y - other.y))
-    //override fun equals(other: Any?) = other is Point && this.x == other.x && this.y == other.y
+    override fun equals(other: Any?) = other is Point && this.x == other.x && this.y == other.y
 }
 
 data class Segment(val begin: Point, val end: Point) {
@@ -25,46 +26,6 @@ data class Segment(val begin: Point, val end: Point) {
 
     override fun hashCode() =
         begin.hashCode() + end.hashCode()
-}
-
-class Line private constructor(val b: Double, val angle: Double) {
-    init {
-        require(angle >= 0 && angle < PI) { "Incorrect line angle: $angle" }
-    }
-
-    constructor(point: Point, angle: Double) : this(point.y * cos(angle) - point.x * sin(angle), angle)
-
-    /**
-     * Средняя (3 балла)
-     *
-     * Найти точку пересечения с другой линией.
-     * Для этого необходимо составить и решить систему из двух уравнений (каждое для своей прямой)
-     */
-    fun crossPoint(other: Line): Point {
-        if (abs(this.angle - PI / 2) <= 1e-5) return Point(
-            -this.b,
-            (-this.b * sin(other.angle) + other.b) / cos(other.angle)
-        )
-        if (abs(other.angle - PI / 2) <= 1e-5) return Point(
-            -other.b,
-            (-other.b * sin(this.angle) + this.b) / cos(this.angle)
-        )
-        val x = (this.b / cos(this.angle) - other.b / cos(other.angle)) / (tan(other.angle) - tan(this.angle))
-        val y = (x * sin(this.angle) + this.b) / cos(this.angle)
-        return Point(x, y)
-    }
-
-    fun angleBetweenLines(other: Line): Double = abs(other.angle - this.angle)
-
-    override fun equals(other: Any?) = other is Line && angle == other.angle && b == other.b
-
-    override fun hashCode(): Int {
-        var result = b.hashCode()
-        result = 31 * result + angle.hashCode()
-        return result
-    }
-
-    override fun toString() = "Line(${cos(angle)} * y = ${sin(angle)} * x + $b)"
 }
 
 data class ZeroVector(val x: Double, val y: Double) {
@@ -81,36 +42,8 @@ fun Stack<Point>.previous(): Point = this[this.size - 2]
 
 fun toZero(a: Double, precision: Double) = if (abs(a) < precision) 0.0 else a
 
-//проверка на правый поворот a -> b -> c
-fun isLeftTurn(a: Point, b: Point, c: Point): Boolean =
-    //(b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x) >= 0
-    ZeroVector(a, b).angleWith(ZeroVector(b, c)) < PI / 2
-
-//алгоритм Грэхема
-fun getHull(listInput: List<Point>, precision: Double): List<Point> {
-    val p = listInput.minByOrNull { it.y }!!
-    val list =
-        listInput.filter { it != p }.sortedBy { ZeroVector(p, it).angleWith(ZeroVector(1.0, 0.0)) }.toMutableList()
-    //list.add(list.first())// <-last change
-    val hull = Stack<Point>()
-    hull.add(p)
-    hull.add(list[0])
-    for (pi in list) {
-        while (hull.size > 1 && !isLeftTurn(hull.previous(), hull.last(), pi)) hull.pop()
-        hull.push(pi)
-    }
-
-    val hullRaw = hull.map { Point(toZero(it.x, precision), toZero(it.y, precision)) }
-    val hullAns = mutableListOf<Point>()
-    var prev = Point(-1.1, -1.1)
-    for (point in hullRaw) {
-        if (point.distance(prev) > precision) hullAns.add(point)
-        prev = point
-    }
-
-    return hullAns.toList()
-}
-
+fun getRightZeroVector(vector: ZeroVector): ZeroVector = //просто вращаем на 90 против часовой
+    ZeroVector(-vector.y, vector.x)
 
 //парсер данных, на которых возникла ошибка
 fun parse(inputName: String): MutableList<Point> {
@@ -151,46 +84,101 @@ fun diameterOld(vararg points: Point): Segment {
     return maxSegment
 }
 
-fun getRightZeroVector(vector: ZeroVector): ZeroVector = //просто вращаем на 90 против часовой
-    ZeroVector(-vector.y, vector.x)
+//проверка на правый поворот a -> b -> c
+fun isNotRightTurn(a: Point, b: Point, c: Point): Boolean =
+    (b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x) >= 0
+
+fun isLeftTurn(a: Point, b: Point, c: Point): Boolean =
+    (b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x) > 0
+
+//алгоритм Грэхема
+fun getHull(listInput: List<Point>, precision: Double): List<Point> {
+    val p = listInput.minByOrNull { it.y }!!
+    val list =
+        listInput.filter { it != p }.sortedBy { ZeroVector(p, it).angleWith(ZeroVector(1.0, 0.0)) }
+            .map { Point(toZero(it.x, precision), toZero(it.y, precision)) }
+    //тонкий момент. далее нужно убрать подряд идущие, и если ох угол через р о остью OX будет одинаковым, то взять самое дальнее
+    //sortedBy - stable, сохраняет порядок одинаковых элементов
+    val hull = Stack<Point>()
+    hull.add(p)
+    hull.add(list[0])
+    for (index in 1 until list.size) {
+        val pi = list[index]
+        while (
+            hull.last().distance(pi) < delta ||
+            !isNotRightTurn(hull.previous(), hull.last(), pi)
+        ) println(hull.pop())
+        hull.push(pi)
+    }
+    //удаление всяких 1e-324
+    val hullAns = mutableListOf<Point>()
+    var prev = hull[0]
+    hullAns.add(prev)
+    for (point in hull) {
+        if (point.distance(prev) > precision) hullAns.add(point)
+        prev = point
+    }
+    //удаление идущих в ряд
+    println(hullAns)
+    var i = 1
+    while (i < hullAns.size - 1) {
+        if (!isLeftTurn(hullAns[i - 1], hullAns[i], hullAns[i + 1])) hullAns.removeAt(i) else i++
+    }
+    println(hullAns)
+    return hullAns.toList()
+    //return hull
+}
 
 fun diameter(vararg points: Point): Segment {
-    //hull
-    if (points.size < 2) throw IllegalArgumentException()
-    if (points.size == 2) return Segment(points[0], points[1])
     val hull = getHull(points.toList(), delta)
-
     var pointIndex = 0
-    var oppositeIndex = hull.indices.maxByOrNull { i -> hull[0].distance(hull[i]) }!!
+    var oppositeIndex = hull.indices.maxByOrNull { i -> hull[i].y }!!
 
     //var sumAngle = 0.0
 
     var result = Segment(hull[0], hull[1])
     var max = result.length()
+    var calipersAngle = 0.0
+
+    fun checkMax(point: Point, opposite: Point) {
+        val athwart = Segment(point, opposite)
+        if (athwart.length() > max) {
+            max = athwart.length()
+            result = athwart
+        }
+    }
+
     while (pointIndex < hull.size) { //движемся против часовой стрелки
         val point = hull.getFromPos(pointIndex)
         val opposite = hull.getFromPos(oppositeIndex)
         val nextPoint = hull.getFromPos(pointIndex + 1)
         val nextOpposite = hull.getFromPos(oppositeIndex + 1)
-        val athwart = Segment(point, opposite)
-        val pointVector = getRightZeroVector(ZeroVector(opposite, point))// opposite -> point, не наоборот
-        val oppositeVector = getRightZeroVector(ZeroVector(point, opposite))
         val pointVectorMoveTo = ZeroVector(point, nextPoint)
         val oppositeVectorMoveTo = ZeroVector(opposite, nextOpposite)
-        if (athwart.length() > max) {
-            max = athwart.length()
-            result = athwart
-        }
-        //println("${pointIndex % hull.size} ${oppositeIndex % hull.size}")
-        val pointAngle = pointVector.angleWith(pointVectorMoveTo)
-        val oppositeAngle = oppositeVector.angleWith(oppositeVectorMoveTo)
+        checkMax(point, opposite)
+        println("${pointIndex % hull.size} ${oppositeIndex % hull.size}")
+        var pointAngleMoveTo = pointVectorMoveTo.angleWith(ZeroVector(1.0, 0.0))
+        if (pointVectorMoveTo.y < 0) pointAngleMoveTo = 2 * PI - pointAngleMoveTo
+        var oppositeAngleMoveTo = oppositeVectorMoveTo.angleWith(ZeroVector(1.0, 0.0))
+        if (oppositeVectorMoveTo.y < 0) oppositeAngleMoveTo = 2 * PI - oppositeAngleMoveTo
+        val pointAngle = (PI + pointAngleMoveTo - calipersAngle) % PI
+        val oppositeAngle = (PI + oppositeAngleMoveTo - (calipersAngle + PI) % (PI * 2)) % PI
         when {
-            abs(pointAngle - oppositeAngle) < delta -> {
+            abs(pointAngle - oppositeAngle) < delta * delta -> {
                 pointIndex++
                 oppositeIndex++
+                checkMax(nextPoint, opposite)
+                checkMax(point, nextOpposite)
+                calipersAngle += pointAngleMoveTo
             }
-            pointAngle < oppositeAngle -> pointIndex++
-            pointAngle > oppositeAngle -> oppositeIndex++
+            pointAngle < oppositeAngle -> {
+                pointIndex++
+                calipersAngle = pointAngleMoveTo
+            }
+            pointAngle > oppositeAngle -> {
+                oppositeIndex++
+                calipersAngle = oppositeAngleMoveTo
+            }
         }
     }
     //println(sumAngle / PI * 180)
@@ -198,14 +186,33 @@ fun diameter(vararg points: Point): Segment {
 }
 
 fun main() {
-    val hull = getHull(parse("input/inputAnswer3.txt"), delta)
-    println(hull)
-    val diameter = diameter(*parse("input/inputAnswer3.txt").toTypedArray())
-    val diameterOld = diameterOld(*parse("input/inputAnswer3.txt").toTypedArray())
+    /*while (true) {
+        val list = List(200) { Point(nextDouble(-1.0, 1.0), nextDouble(-1.0, 1.0)) }
+        val diameter = diameter(*list.toTypedArray())
+        val diameterOld = diameterOld(*list.toTypedArray())
+        println(abs(diameter.length() - diameterOld.length()) < delta)
+    }*/
+    //val list = List(20000) { Point(nextDouble(-1000.0, 1000.0), nextDouble(-1000.0, 1000.0)) }
+    val list = parse("input/inputAnswer.txt")
+    val hull = getHull(list, delta)
+    //println(hull)
+    println("-----------------")
+    val diameter = diameter(*list.toTypedArray())
+    println(diameter)
     println(diameter.length())
     println("${hull.indexOf(diameter.begin)} ${hull.indexOf(diameter.end)}")
+    val diameterOld = diameterOld(*list.toTypedArray())
+    println(diameterOld)
     println(diameterOld.length())
-    println("${hull.indexOf(diameterOld.begin)} ${hull.indexOf(diameterOld.end)}")
+    println(
+        "${
+            hull.indexOf(
+                diameterOld.begin.let { Point(toZero(it.x, delta), toZero(it.y, delta)) })
+        } ${
+            hull.indexOf(
+                diameterOld.end.let { Point(toZero(it.x, delta), toZero(it.y, delta)) })
+        }"
+    )
     println("------------------------------------------")
 }
 
