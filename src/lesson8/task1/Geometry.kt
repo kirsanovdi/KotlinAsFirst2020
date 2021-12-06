3dffddc2fd467d20e01bebfb507b2415b19652bf
kirsanovdi <kirsanov.di@edu.spbstu.ru>
2021-12-07 00:28:55 +0300

    lesson 8

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -84,7 +84,7 @@ data class Circle(val center: Point, val radius: Double) {
      *
      * Вернуть true, если и только если окружность содержит данную точку НА себе или ВНУТРИ себя
      */
-    fun contains(p: Point): Boolean = this.center.distance(p) <= radius + delta
+    fun contains(p: Point): Boolean = this.center.distance(p) <= radius + delta * 100
 }
 
 @Suppress("MemberVisibilityCanBePrivate")
